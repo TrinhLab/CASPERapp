d2a78e6b0ed55991b28c7ebc35531d0a08502078
TannerFry <tfry2@vols.utk.edu>
2020-06-30 13:44:33 -0500

    NCBI search fix for NewGenome

--- NCBI_API.py
+++ NCBI_API.py
@@ -65,7 +65,7 @@ class Assembly:
         for ret in myidlist:
             # this calls Entrez function
             handle = Entrez.esummary(db="assembly", id=ret)
-            record = Entrez.read(handle)
+            record = Entrez.read(handle, validate=False)
 
             # get the orgID which is the ID for the genbank or Refseq link. This could be different than the accession link
             if database == 'RefSeq':
@@ -248,6 +248,8 @@ class Assembly:
         writeStream.close()
         print("\tDone Decompressing")
         return(storeFileName)
+
+
 # testing below
 #myNCBI = Assembly()
 #myNCBI.get_annotation_file("Bacillus Subtilis[Organism]", "GenBank")
