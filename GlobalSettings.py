54368af0ce58a391aa57b996b20590c0132ae37c
TannerFry <tfry2@vols.utk.edu>
2021-07-12 13:07:26 -0500

    forgot to attach to last commit

--- GlobalSettings.py
+++ GlobalSettings.py
@@ -5,12 +5,6 @@ import logging
 # setup logger in global space
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-fh = logging.FileHandler('CASPER.log', mode='w')
-fh_formatter = logging.Formatter('%(asctime)s %(levelname)s %(lineno)d:%(filename)s(%(process)d) - %(message)s')
-fh.setFormatter(fh_formatter)
-fh.setLevel(logging.DEBUG)
-logger.addHandler(fh)
-
 
 OPERATING_SYSTEM_ID = ""
 
